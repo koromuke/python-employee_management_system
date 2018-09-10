07f5d9ee880018bf4e9a9b71d2a41f59a90f9645
koromuke <koromuke@gmail.com>
2018-09-10 23:55:19 +0900

    python club done.

--- secondProject/employee_management_system/admin.py
+++ secondProject/employee_management_system/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Department, Employee, Club
 
-# Register your models here.
+admin.site.register(Department)
+admin.site.register(Employee)
+admin.site.register(Club)
--- secondProject/employee_management_system/views.py
+++ secondProject/employee_management_system/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Employee
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    model = Employee
+    template_name = 'employee/employee_list.html'
